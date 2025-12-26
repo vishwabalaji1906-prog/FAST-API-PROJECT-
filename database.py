540a1c3ad061c4816ed4815c5339f60e8a9704b7
vishwabalaji1906-prog <vishwabalaji1906@gmail.com>
2025-12-26 11:05:58 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-db_url ="postgresql://postgres:Mayavishwa@localhost:5432/vishwa"
+db_url ="postgresql://postgres:123456@localhost:5432/vishwa"
 engine=create_engine(db_url)
+
 
 session=sessionmaker(autoflush=False,autocommit=False,bind=engine)
